43bd113354986a329763e9092c301a75112dffbe
SimoneFrammartino <104621723+SimoneFrammartino@users.noreply.github.com>
2022-05-04 13:36:43 +0200

    add change

--- esercizio meteo/js/myscript.js
+++ esercizio meteo/js/myscript.js
@@ -1,6 +1,14 @@
 document.addEventListener("click",getWeather());
-async function getWeather (){
-    var response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=20&lon=20&appid=0bb4245be51d62b696d4a937929aa03c',{method: GET});
-    var jsonObj = await response.json();
-    window.alert(jsonObj);
-}
+async function getWeather(){
+    let response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4642&lon=9.19&appid=e21c453d380f0ca1bc5d071698438e15&units=metric",{
+    method:"GET"
+    });
+    
+    //fetch("https://api.openweathermap.org/geo/1.0/direct?q=Milan&appid=e21c453d380f0ca1bc5d071698438e15",{
+    // method:"GET"
+    // });
+    
+    let jsonObj = await response.json();
+    console.log(jsonObj);
+    document.getElementById('weather-temp').innerText= jsonObj.main.temp + "°";
+    }
