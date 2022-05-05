f1b6da2383fc5cc85b09ccd9e3d586a2d42947ee
SimoneFrammartino <104621723+SimoneFrammartino@users.noreply.github.com>
2022-05-05 10:43:38 +0200

    prova

--- esercizio meteo/js/myscript.js
+++ esercizio meteo/js/myscript.js
@@ -1,4 +1,4 @@
-document.addEventListener("click", getWeather());
+body.addEventListener("click", getWeather());
 async function getWeather() {
   let response = await fetch(
     "https://api.openweathermap.org/data/2.5/weather?lat=45.4642&lon=9.19&appid=e21c453d380f0ca1bc5d071698438e15&units=metric",
