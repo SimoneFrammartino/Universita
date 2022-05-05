afae088c2b48dfa66ccd0c21ed9630e25e4a7c05
SimoneFrammartino <104621723+SimoneFrammartino@users.noreply.github.com>
2022-05-05 10:41:49 +0200

    prova

--- esercizio meteo/js/myscript.js
+++ esercizio meteo/js/myscript.js
@@ -1,14 +1,17 @@
-document.addEventListener("click",getWeather());
-async function getWeather(){
-    let response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4642&lon=9.19&appid=e21c453d380f0ca1bc5d071698438e15&units=metric",{
-    method:"GET"
-    });
-    
-    //fetch("https://api.openweathermap.org/geo/1.0/direct?q=Milan&appid=e21c453d380f0ca1bc5d071698438e15",{
-    // method:"GET"
-    // });
-    
-    let jsonObj = await response.json();
-    console.log(jsonObj);
-    document.getElementById('weather-temp').innerText= jsonObj.main.temp + "°";
+document.addEventListener("click", getWeather());
+async function getWeather() {
+  let response = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?lat=45.4642&lon=9.19&appid=e21c453d380f0ca1bc5d071698438e15&units=metric",
+    {
+      method: "GET",
     }
+  );
+
+  //fetch("https://api.openweathermap.org/geo/1.0/direct?q=Milan&appid=e21c453d380f0ca1bc5d071698438e15",{
+  // method:"GET"
+  // });
+
+  let jsonObj = await response.json();
+  window.alert(jsonObj);
+  document.getElementById("weather-temp").innerText = jsonObj.main.temp + "°";
+}
